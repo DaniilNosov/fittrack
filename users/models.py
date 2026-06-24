2f6cbbe0de4d2122598dbcf8caf65e79ea057690
Daniil Nosov <nosov281@gmail.com>
2026-06-24 17:19:39 +0300

    Created models in users: Athelte and Fitness level, in workouts: workouttype and workout

--- users/models.py
+++ users/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class FitnessLevel(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+
+class Athlete(AbstractUser):
+    fitness_level = models.ForeignKey(
+        FitnessLevel,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="athletes",
+    )
+    bio = models.TextField(blank=True)
+
+
+    def __str__(self):
+        return self.username
