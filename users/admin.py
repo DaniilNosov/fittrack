35c0f674573247392dc2e88f3c440adadb02fb23
Daniil Nosov <nosov281@gmail.com>
2026-06-24 20:51:30 +0300

    Added for users and workouts: admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from users.models import FitnessLevel, Athlete
+
+
+@admin.register(FitnessLevel)
+class FitnessLevelAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    search_fields = ["name"]
+
+
+@admin.register(Athlete)
+class AthleteAdmin(UserAdmin):
+    list_display = ["username", "email", "first_name", "last_name", "fitness_level"]
+    list_filter = ["fitness_level"]
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("fitness_level", "bio")}),
+    )
